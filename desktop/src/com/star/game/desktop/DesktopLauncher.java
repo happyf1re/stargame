1d162370268978744e7ecdef9f721f61cf541ff7
Dmitry Muravlev <dmur1991@gmail.com>
2021-10-12 17:54:12 +0300

    lesson 6

--- desktop/src/com/star/game/desktop/DesktopLauncher.java
+++ desktop/src/com/star/game/desktop/DesktopLauncher.java
@@ -2,7 +2,7 @@ package com.star.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.star.app.StarGame;
+import com.star.game.StarGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
